a73224cb318734326a7458feaa3c1d8efb698692
yuri <yurigerber.g18@gmail.com>
2021-08-22 14:28:50 +0300

    Update

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ function computerPlay() {
   return playOptions[Math.floor(Math.random() * playOptions.length)];
 }
 
+let playerWins = 0;
+let computerWins = 0;
+
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
 
@@ -14,27 +17,44 @@ function playRound(playerSelection, computerSelection) {
       if (computerSelection === "rock") {
         return "Its a tie! Rock = Rock!";
       } else if (computerSelection === "scissors") {
+        playerWins += 1;
         return "You won! Rock beats scissors!";
       } else if (computerSelection === "paper") {
+        computerWins += 1;
         return "You lose! Paper beats Rock!";
       }
     case "paper":
       if (computerSelection === "paper") {
         return "Its a tie! Paper = Paper!";
       } else if (computerSelection === "rock") {
+        playerWins += 1;
         return "You won! Paper beats rock!";
       } else if (computerSelection === "scissors") {
+        computerWins += 1;
         return "You lose! Scissors beats Paper!";
       }
     case "scissors":
       if (computerSelection === "scissors") {
         return "Its a tie! Scissors = Scissors!";
       } else if (computerSelection === "paper") {
+        playerWins += 1;
         return "You won! Scissors beats paper";
       } else if (computerSelection === "rock") {
+        computerWins += 1;
         return "You lose! Rock beats scissors";
       }
   }
 }
 
-function game() {}
+function game() {
+  for (let i = 1; i <= 5; i++) {
+    console.log(
+      playRound(prompt("Enter: Rock, Paper, Scissors: "), computerPlay()),
+      `\nPlayer wins: ${playerWins} \nComputer wins: ${computerWins}`
+    );
+  }
+
+  playerWins > computerWins
+    ? console.log("You won! :)")
+    : console.log("You lost! :(");
+}
